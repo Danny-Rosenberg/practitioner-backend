3aa0a4c3d5307fb40b2a43a865095ac9c82a66f8
Stewartcm913 <61325840+Stewartcm913@users.noreply.github.com>
2021-02-19 15:03:28 -0500

    Created-contentService

---
+++ src/services/contentService.js
@@ -0,0 +1,19 @@
+var Content = require('../../src/models/practitioner');
+
+exports.createContent = async function(body, practitioner) {
+
+  var content = new Content({
+		about:			 body.about,
+		location:		 body.location,
+		home:			 body.home
+	});
+
+	try {
+		let res = await content.save();
+		console.log('saved content');
+		return res;
+  } catch(err) {
+    console.log('hit error saving content: ',  err);
+    throw err;
+  }
+}
