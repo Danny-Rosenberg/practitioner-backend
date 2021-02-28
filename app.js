8fce63a0a2e5cafbc910c8a9f2cb6400d81b49fc
Daniel Rosenberg <rosenbergd36@gmail.com>
2021-02-28 10:21:10 -0500

    add chai assertion package, add start script for dev mode

--- app.js
+++ app.js
@@ -86,6 +86,6 @@ var server = app.listen(port, function () {
   var host = server.address().address
   var port = server.address().port
    
-	console.log("Example app listening at http://%s:%s", host, port)
+	console.log("app listening at http://%s:%s", host, port)
 	console.log("client host: " + process.env.CLIENT_HOST)
 })
